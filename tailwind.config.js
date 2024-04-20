f154961f37a1c1d6918c631ae85ea44e9f85407f
Ashish Singh <ashishsingh92003@gmail.com>
2024-04-20 13:22:29 +0530

    created the react app with tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default  {
+  content: [
+    "./index.html",
+    "./src/**/*.{jsx,ts,tsx,html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
